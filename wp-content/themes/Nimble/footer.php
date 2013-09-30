84147296a630debff9a08454d173de94ac1b5cae
Juan Torres <juanin.torres@gmail.com>
2013-09-30 18:30:26 -0300

    Cambiando el footer

--- wp-content/themes/Nimble/footer.php
+++ wp-content/themes/Nimble/footer.php
@@ -3,7 +3,7 @@
 
 		<div id="footer-bottom">
 			<div class="container">
-				<p id="copyright"><?php printf( __( 'Designed by %1$s | Powered by %2$s', 'Nimble' ), '<a href="http://www.elegantthemes.com" title="Premium WordPress Themes">Elegant Themes</a>', '<a href="http://www.wordpress.org">WordPress</a>' ); ?></p>
+				<p id="copyright">Diseñado para <a href="http://liki.cl">liki.cl</a> | Optimizado por <a href="http://www.juanintorres.cl" target="_blank">JuaninTorres.cl</a></p>
 			</div> <!-- end .container -->
 		</div> <!-- end #footer-bottom -->
 	</footer> <!-- end #main-footer -->
